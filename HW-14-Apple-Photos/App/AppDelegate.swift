f25297ff42b27e601868fe142de59f6e8b6e5327
Arthur Sh <arthur.shemshur@gmail.com>
2024-02-02 20:51:06 +0200

    - base setup

---
+++ HW-14-Apple-Photos/App/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  HW-14-Apple-Photos
+//
+//  Created by Arthur Sh on 02.02.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
+
---
+++ HW-14-Apple-Photos/App/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  HW-14-Apple-Photos
+//
+//  Created by Arthur Sh on 02.02.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        
+        let viewController = ViewController()
+        
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+
+}
+
