f103dfada9a33627cef847ff3a4073f88b1e8f6f
Arthur Sh <arthur.shemshur@gmail.com>
2024-02-04 09:30:23 +0200

    - add model folder and assets uploaded

---
+++ HW-14-Apple-Photos/Sources/Modules/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  HW-14-Apple-Photos
+//
+//  Created by Arthur Sh on 02.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Ui
+    
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    // MARK: - Setup
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+
+    
+    // MARK: - Actions
+
+}
+
+#Preview {
+    ViewController()
+}
+
